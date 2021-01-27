5d7ab5d6964e098f4ade638735d2c798a232aa0f
Truong Hai Dang <haidanghth910@gmail.com>
2021-01-27 12:08:31 +0700

    all function

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts (' https://www.gstatic.com/firebasejs/8.2.3/firebase-app.js ');
-importScripts (' https://www.gstatic.com/firebasejs/8.2.3/firebase-messaging.js ');
+importScripts('https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.4/firebase-messaging.js');
 
 var firebaseConfig = {
     apiKey: "AIzaSyCVc4J5R1wJX96V3m495qZmHHaMnx_xKSI",
@@ -15,10 +15,12 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function(payload) {
-    const title = "Hai Dang";
-    const options = {
-        body: payload
-    }
-    return self.registration.showNotification(title, options)
-})
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.data.name;
+    const notificationOptions = {
+        body: payload.data.name
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,50 @@
 const express = require('express');
-var admin = require("firebase-admin");
-var serviceAccount = require("../vendingmachine-f9709-firebase-adminsdk-n8hns-a8226448fc.json");
+const fcm = require("../lib/fcm-module");
 const index = express();
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://vendingmachine-f9709.firebaseio.com"
-});
 
 index.post('/ind', (req, res) => {
     res.send("ok")
+})
+
+index.post('/oneDevice', (req, res) => {
+    fcm.messagingOneDevice(req.body.registrationToken, req.body.payload)
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            res.send(error)
+        })
+})
+
+index.post('/multiDevices', (req, res) => {
+    res.send(req.body.registrationToken)
+    // fcm.messagingMultiDevice()
+    //     .then((result) => {
+            
+    //     })
+    //     .catch((error) => {
+            
+    //     })
+})
+
+index.post('/topic', (req, res) => {
+    fcm.messagingTopic()
+        .then((result) => {
+            
+        })
+        .catch((error) => {
+            
+        })
+})
+
+index.post('/multiMess', (req, res) => {
+    fcm.messagingMultiMess()
+        .then((result) => {
+            
+        })
+        .catch((error) => {
+            
+        })
 })
 
 module.exports = index
